fbd3af0eb5b01fe0d0faeac6c591fb4167b55c9c
Sangh0 <hop7311@naver.com>
2022-10-31 14:57:24 +0900

    update ohem ce loss

--- util/loss.py
+++ util/loss.py
@@ -3,14 +3,14 @@ import torch.nn as nn
 
 class OhemCELoss(nn.Module):
 
-    def __init__(self, thresh, ignore_index=255):
+    def __init__(self, thresh, ignore_lb=255):
         super(OhemCELoss, self).__init__()
-        self.thresh = -torch.log(torch.tensor(thresh, requires_grad=False, dtype=torch.float())).cuda()
-        self.ignore_index = ignore_index
-        self.criteria = nn.CrossEntropyLoss(ignore_index=ignore_index, reduce='none')
+        self.thresh = -torch.log(torch.tensor(thresh, requires_grad=False, dtype=torch.float)).cuda()
+        self.ignore_lb = ignore_lb
+        self.criteria = nn.CrossEntropyLoss(ignore_index=ignore_lb, reduction='none')
 
     def forward(self, logits, labels):
-        n_min = labels[labels != self.ignore_index].numel() //  16
+        n_min = labels[labels != self.ignore_lb].numel() // 16
         loss = self.criteria(logits, labels).view(-1)
         loss_hard = loss[loss > self.thresh]
         if loss_hard.numel() < n_min:
