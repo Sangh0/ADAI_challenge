99efcc3d12dfa0f02b130ff98e1e4e95dbdce581
Sangh0 <hop7311@naver.com>
2022-11-01 20:12:04 +0900

    update eval.py

--- eval.py
+++ eval.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('/home/hoo7311/anaconda3/envs/pytorch/lib/python3.8/site-packages')
+import os
 import argparse
 import time
 
@@ -8,7 +11,7 @@ from torch.utils.data import DataLoader
 from model.model import OurModel
 from util.metric import Metrics
 from util.loss import OhemCELoss
-from util.preprocess import EvalDataset
+from dataset import SemanticSegmentationDataset, EvalDataset
 
 
 class Evaluation(object):
@@ -17,16 +20,26 @@ class Evaluation(object):
         self,
         path,
         weight_path,
+        preprocess=False,
     ):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         print(f'device: {self.device} ready...')
+        
+        if preprocess:
+            self.dataloader = DataLoader(
+                EvalDataset(path=path),
+                batch_size=1,
+                shuffle=False,
+                drop_last=False,
+            )
 
-        self.dataloader = DataLoader(
-            EvalDataset(path=path),
-            batch_size=1,
-            shuffle=False,
-            drop_last=False,
-        )
+        else:
+            self.dataloader = DataLoader(
+                SemanticSegmentationDataset(path=path, subset='valid'),
+                batch_size=1,
+                shuffle=False,
+                drop_last=False,
+            )
 
         self.model = OurModel(aux_mode='train', weight_path=None, num_classes=18)
         self.model.load_state_dict(torch.load(weight_path, map_location=torch.device('cpu')))
@@ -69,12 +82,18 @@ def get_args_parser():
                         help='the directory where your dataset is located')
     parser.add_argument('--num_classes', type=int, default=28,
                         help='the number of classes in dataset')
+    parser.add_argument('--data_preprocess', type=bool, default=False,
+                        help='data preprocessing')
     return parser
 
 
 def main(args):
         
-    eval = Evaluation(path=args.data_dir, weight_path=args.weight_dir)
+    eval = Evaluation(
+        path=args.data_dir, 
+        weight_path=args.weight_dir, 
+        preprocess=args.data_preprocess,
+    )
     eval.test()
 
 
